cc970460699590b593e1c8c80089c80aa808eb07
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-08-26 10:29:37 +0300

    editing the config files to allow local db access

--- include/dbconn.php
+++ include/dbconn.php
@@ -1,11 +1,17 @@
 <?php
 /* Database config */
 $db_host		= 'localhost';
-$db_user		= 'id7532504_root';
-$db_pass		= 'uccbwaise2018';
-$db_database	= 'id7532504_ucc'; 
+$db_user		= 'root';
+$db_pass		= '';
+$db_database	= 'ucc'; 
 /* End config */
 
+/* Database config for ooowebhostapp*/
+/*$db_host		= 'localhost';
+$db_user		= 'id7532504_root';
+$db_pass		= 'uccbwaise2018';
+$db_database	= 'id7532504_ucc';
+End config */
 $con = mysqli_connect($db_host, $db_user, $db_pass, $db_database);
 // Check connection
 if (!$con) {
